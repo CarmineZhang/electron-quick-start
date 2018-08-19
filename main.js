d890dd0cb4f036f1789a6f44ace2d619a4d1bc89
zhangmin <zhrm1984@163.com>
2018-08-19 21:22:15 +0800

    修改

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, autoUpdater } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -16,7 +16,7 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -30,7 +30,7 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -38,7 +38,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -48,3 +48,48 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// require('update-electron-app')({
+//   repo: 'CarmineZhang/electron-quick-start',
+//   updateInterval: '5 minutes',
+//   logger: require('electron-log')
+// })
+
+const server = 'https://update.electronjs.org'
+const feed = `${server}/CarmineZhang/electron-quick-start
+/${process.platform}/${app.getVersion()}`
+
+console.log(`Current version: ${app.getVersion()}`)
+
+autoUpdater.setFeedURL(feed)
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 1 * 60 * 1000)
+
+autoUpdater.on('checking-for-update', () => {
+  console.log('checking-for-update')
+})
+
+autoUpdater.on('update-available', () => {
+  console.log('update-available')
+})
+
+autoUpdater.on('update-not-available', () => {
+  console.log('update-not-available')
+})
+
+autoUpdater.on(
+  'update-downloaded',
+  (event, releaseNotes, releaseName, updateURL) => {
+    console.log('update-downloaded', {
+      event,
+      releaseNotes,
+      releaseName,
+      updateURL
+    })
+  }
+)
+
+autoUpdater.on('error', error => {
+  console.log('error', { error })
+})
